98f0646d02e5aa249011dbfa69665378aa1cd3ce
Kevin Loo <kevin_loo@live.cn>
2021-02-06 23:11:23 +0800

    implement-strstr

--- implement-strstr/src/Solution.java
+++ implement-strstr/src/Solution.java
@@ -1,13 +1,20 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle == null || needle.length() == 0) return 0;
-        int fence = 0, length = needle.length();
+        if (needle == null) return 0;
+        int length = needle.length();
+        if (length == 0) return 0;
+        if (haystack.length() < length) return -1;
+        boolean found;
         for (int i = 0, l = haystack.length(); i < l; ++i) {
-            if (haystack.charAt(i) == needle.charAt(fence)) {
-                if (++fence == length) return i - length + 1;
-            } else {
-                fence = 0;
+            if (l - i < length) break;
+            found = true;
+            for (int j = 0; j < length; ++j) {
+                if (needle.charAt(j) != haystack.charAt(j + i)) {
+                    found = false;
+                    break;
+                }
             }
+            if (found) return i;
         }
         return -1;
     }
